8239d5d319238e34f945b4e4ad5c9b163b597c7f
Jasmine Pok <socheatapok0801@gmail.com>
2024-10-29 21:58:03 -0500

    Added method to validate that amount entered is positive

--- budgeting_backend/budget/serializers.py
+++ budgeting_backend/budget/serializers.py
@@ -3,20 +3,40 @@ from .models import Income, EssentialExpenses, NonEssentialExpenses, SavingsGoal
 
 class IncomeSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Income
+        model = Income  # Tie this serializer to the Income model
         fields = ['id', 'user', 'amount', 'name']
+    
+    def validate_amount(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Income amount must be positive.")
+        return value
 
 class EssentialExpensesSerializer(serializers.ModelSerializer):
     class Meta:
-        model = EssentialExpenses
+        model = EssentialExpenses   # Tie this serializer to the Essential Expenses Model
         fields = ['id', 'user', 'amount', 'name']
+
+    def validate_amount(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Expenses amount must be positive.")
+        return value
 
 class NonEssentialExpensesSerializer(serializers.ModelSerializer):
     class Meta:
-        model = NonEssentialExpenses
+        model = NonEssentialExpenses    # Tie this serializer to the Non Essential Expenses model
         fields = ['id', 'user', 'amount', 'name']
+
+    def validate_amount(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Expenses amount must be positive.")
+        return value
 
 class SavingsGoalSerializer(serializers.ModelSerializer):
     class Meta:
-        model = SavingsGoal
+        model = SavingsGoal # Tie this serializer to the SavingsGoal model
         fields = ['id', 'user', 'amount']
+
+    def validate_amount(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Savings Goal amount must be positive.")
+        return value
