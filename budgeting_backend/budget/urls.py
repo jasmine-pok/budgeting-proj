ded62c2e5b4bd8e85a6f4b8f4ea578875608e0ca
Jasmine Pok <socheatapok0801@gmail.com>
2024-11-11 13:48:51 -0600

    Updated urls.py with a few missing slashes

--- budgeting_backend/budget/urls.py
+++ budgeting_backend/budget/urls.py
@@ -14,15 +14,15 @@ URL_PATTERNS = [
     path('income/<int:pk>/', IncomeDetailView.as_view(), name='income-detail'),
 
     # Essential Expenses URLS
-    path('essential-expenses', EssentialExpensesListCreateView.as_view(), name='essential-expenses-list-create'),
+    path('essential-expenses/', EssentialExpensesListCreateView.as_view(), name='essential-expenses-list-create'),
     path('essential-expenses/<int:pk>/', EssentialExpensesDetailView.as_view(), name='essential-expenses-detail'),
 
     # Non Essential Expenses URLS
-    path('non-essential-expenses', NonEssentialExpensesListCreateView.as_view(), name='non-essential-expenses-list-create'),
+    path('non-essential-expenses/', NonEssentialExpensesListCreateView.as_view(), name='non-essential-expenses-list-create'),
     path('non-essential-expenses/<int:pk>/', NonEssentialExpensesDetailView.as_view(), name='non-essential-expenses-detail'),
 
     # Savings Goal
-    path('savings-goal', SavingsGoalListCreateView.as_view(), name='savings-goal-list-create'),
+    path('savings-goal/', SavingsGoalListCreateView.as_view(), name='savings-goal-list-create'),
     path('savings-goal/<int:pk>/', SavingsGoalDetailView.as_view(), name='savings-goal-detail')
 
 ]
