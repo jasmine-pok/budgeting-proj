12f5a752919c955250333072f6d85527c19643e7
Jasmine Pok <socheatapok0801@gmail.com>
2024-11-11 14:17:59 -0600

    Added pagination code to views.py for better performance

--- budgeting_backend/budget/views.py
+++ budgeting_backend/budget/views.py
@@ -4,6 +4,13 @@ from .models import Income, EssentialExpenses, NonEssentialExpenses, SavingsGoal
 from .serializers import IncomeSerializer, EssentialExpensesSerializer, NonEssentialExpensesSerializer, SavingsGoalSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import serializers
+from rest_framework.pagination import PageNumberPagination
+
+# Pagination class to laod 10 records per page 
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 # ListCreateAPIView: lists all incomes for the user and creates new income record
 # GET request to /income/ = list income records
@@ -12,6 +19,7 @@ class IncomeListCreateView(generics.ListCreateAPIView):
     queryset = Income.objects.all() # Get all income records from the database
     serializer_class = IncomeSerializer  # IncomeSerializer will transform Income objects to JSON and vice-versa
     permission_classes = [IsAuthenticated]  # Only authenticated users can access this view
+    pagination_class = StandardResultsSetPagination
 
     # Override get_queryset to filter incomes by the logged-in user
     def get_queryset(self):
@@ -42,6 +50,7 @@ class EssentialExpensesListCreateView(generics.ListCreateAPIView):
     queryset = EssentialExpenses.objects.all()
     serializer_class = EssentialExpensesSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
@@ -66,6 +75,7 @@ class NonEssentialExpensesListCreateView(generics.ListCreateAPIView):
     queryset = NonEssentialExpenses.objects.all()
     serializer_class = NonEssentialExpensesSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
@@ -89,6 +99,7 @@ class SavingsGoalListCreateView(generics.ListCreateAPIView):
     queryset = SavingsGoal.objects.all()
     serializer_class = SavingsGoalSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
