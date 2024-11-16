91b14005a75b7f8703405ab4765ade19bac8dff4
Jasmine Pok <socheatapok0801@gmail.com>
2024-11-16 14:59:10 -0600

    update urls.py for JWT authentication

--- budgeting_backend/user_auth/urls.py
+++ budgeting_backend/user_auth/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from .views import register_user, user_login, user_logout
-from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('register/', register_user, name='register'),
     path('login/', user_login, name='user_login'),
-    path('logout/', user_logout, name='logout')
+    path('logout/', user_logout, name='logout'),
+
+    # JWT Token endpoints
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),    # Obtains access and refresh tokens
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),   # Clients send a valid refresh token for a new access token
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
     
 ]
